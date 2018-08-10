f72019b087acecec291c8d4b827cb1bc437f37a2
Kenneth C. Schalk <kcs-github@xorian.net>
2018-08-10 14:50:55 -0400

    Mirror updates, propagate the dual-function modifiers to my Atreus62 layout

--- keyboards/atreus62/keymaps/xorian/keymap.c
+++ keyboards/atreus62/keymaps/xorian/keymap.c
@@ -69,11 +69,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------' `------' `------' `-----------------------------------------'
  */
 [L_QW] = LAYOUT(
-  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_ENT,                    KC_ENT,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_TAB,                    KC_TAB,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LCTL,                   KC_RCTL, KC_H,    KC_J,    KC_K,    KC_L,    _______,
-  MO(L_NB),KC_Z,    KC_X,    KC_C,    KC_V,    KC_LSFT,                   KC_RSFT, KC_B,    KC_N,    KC_M,    TD(TD_L),MO(L_NB),
-  KC_LGUI, MO(L_MS),MO(L_NV),MO(L_SB),MO(L_MQ),KC_LALT, KC_SPC,  KC_BSPC, KC_LALT, MO(L_MQ),MO(L_SB),MO(L_NV),MO(L_MS),KC_RGUI
+  KC_1,       KC_2,       KC_3,       KC_4,    KC_5,    KC_ENT,                    KC_ENT,  KC_6,           KC_7,       KC_8,       KC_9,    KC_0,
+  KC_Q,       KC_W,       KC_E,       KC_R,    KC_T,    KC_TAB,                    KC_TAB,  KC_Y,           KC_U,       KC_I,       KC_O,    KC_P,
+  KC_A,ALT_T(KC_S),CTL_T(KC_D),SFT_T(KC_F),    KC_G,    KC_LCTL,                   KC_RCTL, KC_H,    SFT_T(KC_J),CTL_T(KC_K),ALT_T(KC_L), _______,
+  MO(L_NB),   KC_Z,       KC_X,       KC_C,    KC_V,    KC_LSFT,                   KC_RSFT, KC_B,           KC_N,       KC_M,   TD(TD_L), MO(L_NB),
+  KC_LGUI,MO(L_MS),    MO(L_NV),  MO(L_SB),MO(L_MQ),    KC_LALT, KC_SPC,  KC_BSPC, KC_LALT, MO(L_MQ),   MO(L_SB),   MO(L_NV),   MO(L_MS),  KC_RGUI
 ),
 
 /* Tarmak 1 layer
