125653884951252d967293ac025478c0e00e4d0f
Kenneth C. Schalk <kcs-github@xorian.net>
2019-03-01 10:53:04 -0500

    Fix tap/hold modifiers so they work in number and symbol layers

--- keyboards/ergodox_ez/keymaps/xorian/keymap.c
+++ keyboards/ergodox_ez/keymaps/xorian/keymap.c
@@ -584,7 +584,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // left hand
        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
        _______, _______, KC_7,    KC_8,    KC_9,    KC_0,    _______,
-       _______, _______, KC_4,    KC_5,    KC_6,    _______,
+       _______, _______, ALT_T(KC_4),CTL_T(KC_5),SFT_T(KC_6),    _______,
        _______, _______, KC_1,    KC_2,    KC_3,    _______, _______,
        _______, _______, _______, _______, _______,
                                            _______, _______,
@@ -593,7 +593,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // right hand
        KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
        _______,  KC_7,    KC_8,    KC_9,    KC_0,    _______, _______,
-                 KC_4,    KC_5,    KC_6,    _______, _______, _______,
+                 KC_4,    SFT_T(KC_5),CTL_T(KC_6),KC_LALT, _______, _______,
        _______,  KC_1,    KC_2,    KC_3,    _______, _______, _______,
                           _______, _______, _______, _______, _______,
        _______, _______,
@@ -668,7 +668,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // left hand
        _______, _______, _______, _______, _______, _______, _______,
        _______, _______, _______, KC_GRV,  KC_MINS, KC_EQL,  _______,
-       _______, _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_QUOT,
+       _______, _______, ALT_T(KC_LBRC),CTL_T(KC_RBRC),SFT_T(KC_SCLN), KC_QUOT,
        _______, _______, KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, _______,
        _______, _______, _______, _______, _______,
                                            _______, _______,
@@ -677,7 +677,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // right hand
        _______,  _______, _______, _______, _______, _______, _______,
        _______,  KC_GRV,  KC_MINS, KC_EQL,  _______, _______, _______,
-                 KC_LBRC, KC_RBRC, KC_SCLN, KC_QUOT, _______, _______,
+                 KC_LBRC, SFT_T(KC_RBRC),CTL_T(KC_SCLN),ALT_T(KC_QUOT), _______, _______,
        _______,  KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, _______, _______,
                           _______, _______, _______, _______, _______,
        _______, _______,
